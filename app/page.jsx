d434432bc7defb65738aa2e8557e993dfa3277de
kinnatzzang <93966256+kinnatzzang@users.noreply.github.com>
2024-01-17 14:49:47 +0900

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet, 
+        consectetur adipisicing elit. 
+        Voluptatum eum labore, 
+        vel impedit quod fugit 
+        voluptates quae, 
+        nobis exercitationem quos eaque tempore incidunt, 
+        quibusdam dolorem soluta sint maxime amet dolor.
+      </p>
+    </div>
+  )
+}
+
+export default Homepage
