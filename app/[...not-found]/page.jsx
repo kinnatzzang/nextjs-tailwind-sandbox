692de79607750538d8bfdf55c7bbca3fb1f4998e
kinnatzzang <93966256+kinnatzzang@users.noreply.github.com>
2024-01-19 18:15:49 +0900

    feat: add General NotFound page

---
+++ app/[...not-found]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div className='bg-green-300'>
+      <h1 className='text-green-500'>NotFound</h1>
+    </div>
+  )
+}
+
+export default Notfound
