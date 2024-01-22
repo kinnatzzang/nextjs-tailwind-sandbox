004897f4d6aaa0ffa98d3ce9580a2f6ac931df27
kinnatzzang <93966256+kinnatzzang@users.noreply.github.com>
2024-01-22 15:54:05 +0900

    refact: refactor Main Layout

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
+    <div className='bg-white rounded-lg mx-4 p-4'>
       <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. 
+        Laboriosam doloribus 
+        id velit illum ipsam vitae nisi aliquid 
+        necessitatibus sit, non quaerat nemo eligendi 
+        tempore amet 
+        obcaecati neque optio et repudiandae.
+      </p>
     </div>
   )
 }
